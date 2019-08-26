0788d4be6d8e02a423c979a8715226f1905cc5b9
rohit j <jrohit072@gmail.com>
2019-08-27 00:00:01 +0530

    update

--- 1.cpp
+++ 1.cpp
@@ -2,30 +2,42 @@
 using namespace std;
 typedef long long ll;
  
-int a[50][50], b[50][50];
+const int nax = 3e5+10;
+ll fac[nax];
+ll mod = 998244353;
+ 
+#define x first
+#define y second
+pair<int,int> p[nax];
+ 
 int main() {
   ios::sync_with_stdio(0); cin.tie(0);
-  int h, w;
-  cin >> h >> w;
-  for (int i = 0; i < h; i++)
-    for (int j = 0; j < w; j++)
-      cin >> a[i][j];
-  vector<pair<int,int>> ans;
-  for (int i = 0; i+1 < h; i++)
-    for (int j = 0; j+1 < w; j++) {
-      if (a[i][j] && a[i+1][j] && a[i][j+1] && a[i+1][j+1]) {
-	ans.push_back({i+1,j+1});
-	b[i][j] = b[i+1][j] = b[i][j+1] = b[i+1][j+1] = 1;
-      }
-    }
-  for (int i = 0; i < h; i++)
-    for (int j = 0; j < w; j++)
-      if (a[i][j] != b[i][j]) {
-	cout << -1 << endl;
-	return 0;
-      }
-  cout << ans.size() << endl;
-  for (auto p : ans) {
-    cout << p.first << ' ' << p.second << endl;
+  fac[0] = 1;
+  for (int i = 1; i < nax; i++) {
+    fac[i] = fac[i-1]*i%mod;
   }
+ 
+  int n;
+  cin >> n;
+  map<int,int> cx, cy;
+  map<pair<int,int>,int> cxy;
+  for (int i = 0; i < n; i++) {
+    cin >> p[i].x >> p[i].y;
+    cx[p[i].x]++;
+    cy[p[i].y]++;
+    cxy[p[i]]++;
+  }
+  ll first = 1, second = 1, both = 1;
+  for (auto p : cx)
+    (first *= fac[p.second]) %= mod;
+  for (auto p : cy)
+    (second *= fac[p.second]) %= mod;
+  sort(p, p+n);
+  for (int i = 1; i < n; i++)
+    if (p[i-1].y > p[i].y)
+      both = 0;
+  for (auto p : cxy)
+    (both *= fac[p.second]) %= mod;
+  //cout << first << ' ' << second << ' ' << both << endl;
+  cout << ((fac[n]-first-second+both)%mod+mod)%mod << endl;
 }
